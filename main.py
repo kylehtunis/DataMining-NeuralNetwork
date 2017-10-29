62758948d7729230e49989ec81981d5d74069172
Unknown <kyle.h.tunis@gmail.com>
2017-10-29 13:44:36 -0400

    main function framework

--- main.py
+++ main.py
@@ -6,5 +6,42 @@ Created on Fri Oct 27 18:21:04 2017
 """
 
 import numpy as np
-import scipy
-import pandas
+import scipy.io.arff as arff
+import sys
+import partition
+import NeuralNet
+import DataTransform
+import evaluate
+
+###get data from file
+fileName=sys.argv[1]
+f=open(fileName, 'r')
+arff.loadarff(f)
+
+###partition
+d = data.copy()
+numpy.random.seed=(80085)
+numpy.random.shuffle(d)
+partitions=partition.partition(d)
+
+###preproces and train 10 models
+models=[]
+for i in range(len(partitions)):
+    print('Training Fold '+str(i+1))
+    test=partitions.pop(0)
+    testCopy=test.copy()
+    train=np.concatenate(partitions).copy()
+    preprocess.preprocess(train, meta)
+    nnData=DataTransform.transform(train, meta)
+    NeuralNet nn(nnData)
+    models.append(nn)
+    partitions.append(testCopy)
+    
+###classify test data
+results[]
+for i in range(len(models)):
+    results.append(m.classify(partitions[i]))
+    
+###evaluate results
+for i in range(len(results)):
+    evaluate.evaluate(results[i], partitions[i])
